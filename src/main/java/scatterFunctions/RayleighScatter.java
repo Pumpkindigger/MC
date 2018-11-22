45f0cc5d84f21d80549bda8c967bc0ab4f5e89fe
Pumpkindigger <zonneveld.noordwijk@gmail.com>
2018-11-22 22:46:59 +0100

    Found source for Rayleigh scatter function to implement

---
+++ src/main/java/scatterFunctions/RayleighScatter.java
@@ -0,0 +1,12 @@
+package scatterFunctions;
+
+/**
+ * TODO: implement rayleigh scattering
+ * http://orbit.dtu.dk/files/6314521/3D9A1d01.pdf
+ */
+public class RayleighScatter extends ScatterFunction {
+
+    public double calculateAngle(double omega){
+       return 0.0;
+    }
+}
