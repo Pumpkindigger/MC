e932d94bf2abe0687fcb35f6f207aa692722fe97
Pumpkindigger <zonneveld.noordwijk@gmail.com>
2019-01-14 22:37:01 +0100

    Rayleigh scatter now used Baranoski’s rejection sampling

--- src/main/java/main/TestMain.java
+++ src/main/java/main/TestMain.java
@@ -3,6 +3,8 @@ package main;
 import main.objects.GasLayerBend2D;
 import main.objects.Photon;
 import scatterFunctions.HenyeyGreensteinScatter;
+import scatterFunctions.RayleighScatter;
+import scatterFunctions.ScatterFunction;
 
 public class TestMain {
 
@@ -10,7 +12,7 @@ public class TestMain {
      *
      */
     public static void main(String[] args){
-        backtrackTest();
+        scatterTest();
     }
 
     private static void databankTest(){
@@ -22,5 +24,16 @@ public class TestMain {
         Photon photon = new Photon(-21.398631044570052, -6.82137225735025, -3.8029701420042796, true);
         photon.setOldCoordinate(new Coordinate(-18.731939427385893, -5.428530163937989, -2.238152582293153));
         System.out.println(photon.backTrack(new GasLayerBend2D(20, 10, 360, 0, 5, 0, new HenyeyGreensteinScatter())));
+    }
+
+    private static void scatterTest(){
+        int count = 100000;
+        double sum = 0.0;
+        ScatterFunction scatter = new RayleighScatter();
+        for(int i = 0; i< count; i++){
+            sum += scatter.calculateAngle(0.0);
+            //System.out.println(scatter.calculateAngle(0.0));
+        }
+        System.out.println(sum);
     }
 }
--- src/main/java/scatterFunctions/RayleighScatter.java
+++ src/main/java/scatterFunctions/RayleighScatter.java
@@ -4,15 +4,16 @@ import main.MyRandom;
 
 /**
  * http://orbit.dtu.dk/files/6314521/3D9A1d01.pdf
+ * Baranoski’s rejection sampling
  */
 public class RayleighScatter extends ScatterFunction {
 
     public double calculateAngle(double omega) {
         double cosTheta;
         do {
-            cosTheta = 2 * MyRandom.random() - 1;
+            cosTheta = Math.cos(Math.PI*MyRandom.random());
         }
-        while (MyRandom.random() > 0.5 * (1 + cosTheta*cosTheta));
+        while (MyRandom.random() > 9/(4*Math.sqrt(6)) * (1+cosTheta*cosTheta) * Math.pow(1-cosTheta*cosTheta, 0.5));
         return cosTheta;
     }
 }
